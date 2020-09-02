f4efa3d5d21bf39c6e76cb86be63c0a738d7d1f0
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2020-09-02 16:25:11 +0300

    Toggle fullscreen from command line

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,28 @@ mod demo;
 mod engine;
 mod scripting;
 
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(
+    version = "0.1.0",
+    author = "Ilkka Hänninen <ilkka.hanninen@gmail.com>"
+)]
+struct Opts {
+    #[clap(short, long)]
+    fullscreen: bool,
+}
+
 fn main() {
+    let opts = Opts::parse();
+
     let mut window = engine::window::Window::new(&engine::window::WindowProperties {
         title: "My demo",
         size: winit::dpi::PhysicalSize {
             width: 1920,
             height: 1080,
         },
-        fullscreen: false,
+        fullscreen: opts.fullscreen,
     });
 
     let engine = demo::init(&mut window.window);
