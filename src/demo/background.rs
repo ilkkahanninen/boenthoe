1a64a1799d4174279dabc14afcc1a7a184dff928
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2020-09-14 19:32:09 +0300

    Lively background

--- src/demo/background.rs
+++ src/demo/background.rs
@@ -1,9 +1,12 @@
 use crate::demo::state::State;
+use crate::engine::object::Object;
 use crate::engine::*;
 use std::rc::Rc;
+use wgpu::util::DeviceExt;
 
 pub struct Background {
     pipeline: wgpu::RenderPipeline,
+    uniforms: BackgroundObject,
     images: [texture::Texture; 9],
     output: Rc<texture::Texture>,
 }
@@ -22,16 +25,24 @@ impl Background {
             texture_builder.diffuse(include_bytes!("assets/kuosi8.jpg"), "kuosi8.jpg"),
             texture_builder.diffuse(include_bytes!("assets/kuosi9.jpg"), "kuosi9.jpg"),
         ];
+        let uniforms = BackgroundObject::new(
+            &engine.device,
+            BackgroundModel {
+                zoom: 0.5,
+                brightness: 1.0,
+            },
+        );
 
         let pipeline = pipeline::PipelineBuilder::new()
-            .vertex_shader(include_str!("shaders/layer.vert"), "layer.vert")
-            .fragment_shader(include_str!("shaders/Background.frag"), "background.frag")
-            .bind_objects(&[&images[0]])
+            .vertex_shader(include_str!("shaders/background.vert"), "background.vert")
+            .fragment_shader(include_str!("shaders/background.frag"), "background.frag")
+            .bind_objects(&[&images[0], &uniforms])
             .add_command_buffers(texture_builder.command_buffers)
             .build(engine);
 
         Box::new(Self {
             pipeline,
+            uniforms,
             images,
             output,
         })
@@ -39,7 +50,11 @@ impl Background {
 }
 
 impl renderer::Renderer<State> for Background {
-    fn update(&mut self, _ctx: &mut renderer::RenderingContext<State>) {}
+    fn update(&mut self, ctx: &mut renderer::RenderingContext<State>) {
+        self.uniforms.model.zoom = ctx.state.time.sin() as f32 * 0.25 + 0.25;
+        self.uniforms.model.brightness = 1.0 - ctx.state.time.fract() as f32;
+        self.uniforms.update(ctx.device, ctx.encoder);
+    }
 
     fn render(&mut self, ctx: &mut renderer::RenderingContext<State>) {
         let mut render_pass = ctx.encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
@@ -63,6 +78,99 @@ impl renderer::Renderer<State> for Background {
 
         render_pass.set_pipeline(&self.pipeline);
         render_pass.set_bind_group(0, &self.images[index].bind_group, &[]);
+        render_pass.set_bind_group(1, &self.uniforms.bind_group, &[]);
         render_pass.draw(0..6, 0..1);
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct BackgroundModel {
+    zoom: f32,
+    brightness: f32,
+}
+
+unsafe impl bytemuck::Pod for BackgroundModel {}
+unsafe impl bytemuck::Zeroable for BackgroundModel {}
+
+struct BackgroundObject {
+    pub model: BackgroundModel,
+    pub buffer: wgpu::Buffer,
+    pub bind_group_layout: wgpu::BindGroupLayout,
+    pub bind_group: wgpu::BindGroup,
+}
+
+impl BackgroundObject {
+    pub fn new(device: &wgpu::Device, model: BackgroundModel) -> Self {
+        let buffer = Self::create_buffer(device, model, true);
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::UniformBuffer {
+                    dynamic: false,
+                    min_binding_size: wgpu::BufferSize::new(
+                        std::mem::size_of::<BackgroundModel>() as _
+                    ),
+                },
+                count: None,
+            }],
+            label: Some("uniform_bind_group_layout"),
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer(buffer.slice(..)),
+            }],
+            label: Some("uniform_bind_group"),
+        });
+
+        Self {
+            model,
+            buffer,
+            bind_group_layout,
+            bind_group,
+        }
+    }
+
+    pub fn update(&self, device: &wgpu::Device, encoder: &mut wgpu::CommandEncoder) {
+        let buffer = Self::create_buffer(device, self.model, false);
+        encoder.copy_buffer_to_buffer(
+            &buffer,
+            0,
+            &self.buffer,
+            0,
+            std::mem::size_of::<BackgroundModel>() as wgpu::BufferAddress,
+        );
+    }
+
+    fn create_buffer(
+        device: &wgpu::Device,
+        model: BackgroundModel,
+        is_destination: bool,
+    ) -> wgpu::Buffer {
+        device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            contents: bytemuck::cast_slice(&[model]),
+            usage: wgpu::BufferUsage::UNIFORM
+                | if is_destination {
+                    wgpu::BufferUsage::COPY_DST
+                } else {
+                    wgpu::BufferUsage::COPY_SRC
+                },
+            label: None,
+        })
+    }
+}
+
+impl Object for BackgroundObject {
+    fn get_layout(&self) -> &wgpu::BindGroupLayout {
+        &self.bind_group_layout
+    }
+
+    fn get_bind_group(&self) -> &wgpu::BindGroup {
+        &self.bind_group
     }
 }
--- src/demo/shaders/background.frag
+++ src/demo/shaders/background.frag
@@ -5,7 +5,21 @@ layout(location=0) out vec4 f_color;
 
 layout(set = 0, binding = 0) uniform texture2D t_diffuse;
 layout(set = 0, binding = 1) uniform sampler s_diffuse;
+layout(set = 1, binding = 0) uniform Uniforms {
+    float u_zoom;
+    float u_brightness;
+};
 
 void main() {
-    f_color = texture(sampler2D(t_diffuse, s_diffuse), v_tex_coords);
+    vec2 zoom = (vec2(0.5, 0.5) - v_tex_coords) * u_zoom; // todo: make this unlinear
+    // vec2 distort = sin(zoom) * length(zoom);
+    vec2 distort = reflect(v_tex_coords, sin(zoom * length(zoom) * 16.0)) * length(zoom) * 0.05;
+    vec3 color = pow(
+        texture(
+            sampler2D(t_diffuse, s_diffuse),
+            v_tex_coords + zoom + distort
+        ).xyz,
+        vec3(1.25 - u_brightness)
+    );
+    f_color = vec4(color, 1.0);
 }
