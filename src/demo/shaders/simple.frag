4d674f8dc2664916369ea0d411336c44db08f2f8
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2020-11-07 20:18:33 +0200

    Time does not update, run this on macbook

--- src/demo/shaders/simple.frag
+++ src/demo/shaders/simple.frag
@@ -3,6 +3,14 @@
 layout(location=0) in vec2 v_tex_coords;
 layout(location=0) out vec4 f_color;
 
+layout(set = 0, binding = 0) uniform SimpleUniforms {
+    float time;
+};
+
 void main() {
-    f_color = vec4(v_tex_coords.x, v_tex_coords.y, (v_tex_coords.x + v_tex_coords.y) * 0.5, 1.0);
+    f_color = vec4(
+        0.5 + sin(v_tex_coords.x * 3.0 + v_tex_coords.y * 4.0 + time * 10.2) * 0.5,
+        0.5 + sin(v_tex_coords.x * 4.0 + v_tex_coords.y * 3.0 + time * 10.3) * 0.5,
+        0.5 + sin(v_tex_coords.x * 2.5 + v_tex_coords.y * 3.5 + time * 10.4) * 0.5,
+        1.0);
 }
--- src/demo/simple.rs
+++ src/demo/simple.rs
@@ -3,6 +3,7 @@ use crate::engine::prelude::*;
 pub struct Simple {
     pipeline: wgpu::RenderPipeline,
     output: Rc<Texture>,
+    uniforms: UniformBuffer<SimpleUniforms>,
 }
 
 impl Simple {
@@ -12,27 +13,44 @@ impl Simple {
             &engine.load_asset(Path::new("shaders/layer.vert")),
             None,
         )?;
-        let fragment_sahder = shaders::build(
+
+        let fragment_shader = shaders::build(
             engine,
             &engine.load_asset(Path::new("shaders/simple.frag")),
             None,
         )?;
 
+        let uniforms = UniformBuffer::new(&engine.device, "SimpleUniforms");
+        let layouts = [uniforms.get_layout()];
+
         let pipeline_descriptor = pipeline::PipelineDescriptor::builder()
             .label("Simple")
             .vertex_shader(&vertex_shader)
-            .fragment_shader(&fragment_sahder)
+            .fragment_shader(&fragment_shader)
+            .bind_group_layouts(&layouts)
             .build();
 
         Ok(Self {
             pipeline: pipeline::build_pipeline(engine, pipeline_descriptor),
             output,
+            uniforms,
         })
     }
 }
 
 impl Renderer for Simple {
-    fn update(&mut self, _context: &mut RenderingContext) {}
+    fn update(&mut self, context: &mut RenderingContext) {
+        let mut encoder = context.create_encoder("update");
+        self.uniforms.copy_to_gpu(
+            &mut encoder,
+            context.queue,
+            &SimpleUniforms {
+                time: context.time as f32,
+            },
+        );
+        println!("Time: {}", context.time);
+        context.submit(encoder);
+    }
 
     fn render(&mut self, context: &mut RenderingContext) {
         let mut encoder = context.create_encoder("Simple");
@@ -50,8 +68,18 @@ impl Renderer for Simple {
             });
 
             render_pass.set_pipeline(&self.pipeline);
+            render_pass.set_bind_group(0, self.uniforms.get_bind_group(), &[]);
             render_pass.draw(0..6, 0..1);
         }
         context.submit(encoder);
     }
 }
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct SimpleUniforms {
+    time: f32,
+}
+
+unsafe impl bytemuck::Zeroable for SimpleUniforms {}
+unsafe impl bytemuck::Pod for SimpleUniforms {}
